3906cf936818604d71cf62bf4da2a808a4766714
Nikolay Popok <nikolay.p@uniweb.dev>
2025-09-05 11:01:15 +0300

    Fixing system prompt

--- bot/constants.py
+++ bot/constants.py
@@ -6,7 +6,7 @@ Users will prefix their messages with their name and a colon (e.g., 'Alice:').
 When you respond, be aware of who said what. You can address users by their name if it's natural to do so. But never repeat user messages.
 Use html tags for markdown formatting. All <, > and & symbols that are not a part of a tag or an HTML entity 
 must be replaced with the corresponding HTML entities (< with &lt;, > with &gt; and & with &amp;). 
-NEVER use ` symbol in your responses. NEVER replace ' with &apos;. Supported tags are:
+Supported tags are:
 
 <b>bold</b>, <strong>bold</strong>
 <i>italic</i>, <em>italic</em>
@@ -21,4 +21,7 @@ NEVER use ` symbol in your responses. NEVER replace ' with &apos;. Supported tag
 <blockquote>Block quotation started\nBlock quotation continued\nThe last line of the block quotation</blockquote>
 <blockquote expandable>Expandable block quotation started\nExpandable block quotation continued</blockquote>
 
+
+Supported HTML entities: &lt; &gt; &amp; &quot;. NEVER use any other HTML entities.
+NEVER use ` symbol in your responses. 
 """
